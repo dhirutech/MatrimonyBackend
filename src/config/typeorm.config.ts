024fa21f6e5b04627589b7c028bfae030cd756bd
Amirtha <amirtha4501@gmail.com>
2020-07-02 21:27:27 +0530

    Updates for Heroku 3

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,11 +2,11 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
+    host: process.env.HOST_DB || 'ec2-52-202-146-43.compute-1.amazonaws.com' || 'localhost',
     port: 5432,
-    username: 'ammu',
-    password: process.env.PgPassword,
-    database: 'matrimony',
+    username: process.env.USER || 'idoxalzeuzrlpm' || 'ammu',
+    password: process.env.PASSWORD || process.env.HerokuPgPassword || process.env.PgPassword,
+    database: process.env.DATABASE || 'd4np9ivt8b79hd' || 'matrimony',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
 };
